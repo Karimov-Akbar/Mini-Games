bcba02b80ac86df2e7a9f8934e9a786c65ce9784
Akbar <akbarkarimov250@gmail.com>
2024-12-29 00:08:34 +0500

    fixes

--- public/js/script.js
+++ public/js/script.js
@@ -86,3 +86,18 @@ document.addEventListener('DOMContentLoaded', () => {
     gameContainer.innerHTML = '';
   }
 });
+
+document.addEventListener('touchmove', function (event) {
+  if (event.scale !== 1) {
+      event.preventDefault();
+  }
+}, { passive: false });
+
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
